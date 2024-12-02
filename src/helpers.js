5ca43be57389d55b1905c1e909970ed026aa0795
shaker <hydrashaker@gmail.com>
2024-12-02 15:38:00 +0530

    8

--- src/helpers.js
+++ src/helpers.js
@@ -1,12 +1,39 @@
+const generateRandomColor = () =>{
+    const existingBudgetLength = fetchData("budgets")?.
+    length ?? 0;
+    return `${existingBudgetLength * 34} 65% 50%`
+}
+
+
+
 //Local storage
 
 import { LockClosedIcon } from "@heroicons/react/24/solid"
 
+//local storage
 export const fetchData = (key) =>{
     return JSON.parse(localStorage.getItem(key))
 }
 
+//create budget
+export const createBudget = ({
+    name, amount
+}) =>{
+    const newItem = {
+        id: crypto.randomUUID(),
+        name: name,
+        createAt: Date.now(),
+        amount: +amount,
+        color: generateRandomColor()
 
+    }
+    const existingBudgets = fetchData("budgets") ?? [];
+    return localStorage.setItem("budgets", 
+        JSON.stringify([...existingBudgets, newItem])
+    )
+}
+
+//delete item
 export const deleteItem = ({ key})=>{
     return localStorage.removeItem(key)
 }

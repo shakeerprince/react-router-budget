fc4fd23d656035e75d199335780dcbd50f25643a
shaker <hydrashaker@gmail.com>
2024-12-02 15:08:53 +0530

    made form

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,11 +3,13 @@ import { fetchData } from '../helpers';
 import { useLoaderData } from 'react-router-dom';
 import Intro from "../components/Intro"
 import { toast } from "react-toastify";
+import AddBudgetForm from "../components/AddBudgetForm";
 
 //loader
 export function dashboardLoader(){
     const userName = fetchData("userName");
-    return { userName}
+    const budgets = fetchData("userName");
+    return { userName, budgets}
 }
 
 //action
@@ -30,7 +32,20 @@ const Dashboard = ()=> {
 
   return (
     <>
-         {userName ? ( <p>{userName}</p>):
+         {userName ? ( 
+          <div className="dashboard">
+            <h1>Welcome back, <span className='accent'>
+              {userName}</span> </h1>
+              <div className="grid-sm">
+              {/* budgets ? () : () */}
+              <div className="grid-lg">
+                <div className="flex-lg">
+                  <AddBudgetForm />
+                </div>
+              </div>
+              </div>
+          </div>
+         ):
          <Intro />}
         
     </>
